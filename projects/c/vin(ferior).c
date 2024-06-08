bd59251b3e6b3cd0a3fc3570fa9fe84426b72369
Tom Delalande <80664757+thomas-delalande@users.noreply.github.com>
2024-06-09 00:12:38 +0800

    [*] feat(c): working vim motions

--- projects/c/vin(ferior).c
+++ projects/c/vin(ferior).c
@@ -1,33 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
 
-#define MAX_ROWS 100
-#define MAX_COLS 100
+#define MAX_ROWS 1000
+#define MAX_COLS 300
 
 void clear() { printf("\033[H\033[J"); }
 
-void print(int cursorRow, int cursorCol, int numRows,
-           char filedata[MAX_ROWS][MAX_COLS]) {
+void save(char *filename, FILE *file, char *filedata) {
+  truncate(filename, 0);
+  fprintf(file, "%s", filedata);
+}
 
-  int rowCounter = 0;
-  for (int i = 0; i <= numRows; i++) {
-    char *data = filedata[i];
-    if (rowCounter == cursorRow) {
-      char clonedData[MAX_COLS];
-      for (int j = 0; j < MAX_COLS; j++) {
-        clonedData[j] = data[j];
-      }
-      clonedData[cursorCol] = '|';
-      printf("%s", clonedData);
+void print(int cursor, char *filedata, int filesize) {
+  /* printf("%s", filedata); */
+  for (int i = 0; i <= filesize; i++) {
+    char c = filedata[i];
+    if (i == cursor) {
+      printf("%s", "█");
     } else {
-      printf("%s", filedata[i]);
+      printf("%c", c);
     }
-    rowCounter++;
   }
 }
 
-void write() {}
-
 int main(int argc, char *argv[]) {
+  struct termios orig_termios, new_termios;
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  new_termios = orig_termios;
+  new_termios.c_lflag &= ~(ICANON | ECHO);
+  tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+
   char *filename = argv[1];
   FILE *file = fopen(filename, "a+");
 
@@ -35,29 +40,26 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
+  fseek(file, 0, SEEK_END);
+  long filesize = ftell(file);
   fseek(file, 0, SEEK_SET);
-  char filedata[MAX_ROWS][MAX_COLS];
+
+  char *filedata = (char *)malloc((filesize + 1) * sizeof(char));
 
   int rows = 0;
-  while (fgets(filedata[rows], MAX_COLS, file) != NULL && rows < MAX_ROWS) {
-    rows++;
-  }
+  fread(filedata, sizeof(char), filesize, file);
 
-  int column = 0;
-  int row = 0;
+  int cursor = 0;
   int mode = 0;
 
   clear();
-  print(row, column, rows, filedata);
+  print(cursor, filedata, filesize);
 
   while (1) {
     char c = getchar();
     if (mode == 0) {
       if (c == 'w') {
-        fseek(file, 0, SEEK_SET);
-        for (int i = 0; i < rows; i++) {
-          fprintf(file, "%s\n", filedata[i]);
-        }
+        save(filename, file, filedata);
       } else if (c == 'a') {
         mode = 1;
       } else {
@@ -66,29 +68,72 @@ int main(int argc, char *argv[]) {
           return 0;
         }
         if (c == 'k') {
-          row -= 1;
+          int currentLineStart = cursor;
+          while (currentLineStart > 0 && filedata[currentLineStart] != '\n') {
+            currentLineStart--;
+          }
+
+          int previousLineStart = currentLineStart - 1;
+          while (previousLineStart > 0 && filedata[previousLineStart] != '\n') {
+            previousLineStart--;
+          }
+
+          if (previousLineStart >= 0) {
+            int previousLineLength = currentLineStart - previousLineStart;
+            int offset = cursor - currentLineStart;
+            cursor = previousLineStart +
+                     ((offset > previousLineLength) ? previousLineLength - 2
+                                                    : offset - 1) +
+                     1;
+          }
         }
         if (c == 'j') {
-          row += 1;
+          int currentLineStart = cursor;
+          while (currentLineStart > 0 && filedata[currentLineStart] != '\n') {
+            currentLineStart--;
+          }
+
+          int nextLineStart = currentLineStart + 1;
+          while (nextLineStart < filesize && filedata[nextLineStart] != '\n') {
+            nextLineStart++;
+          }
+
+          int nextNextLineStart = nextLineStart + 1;
+          while (nextNextLineStart < filesize &&
+                 filedata[nextNextLineStart] != '\n') {
+            nextNextLineStart++;
+          }
+
+          if (nextLineStart < filesize) {
+            int nextLineLength = nextNextLineStart - nextLineStart;
+            int offset = cursor - currentLineStart;
+            cursor =
+                nextLineStart +
+                ((offset > nextLineLength) ? nextLineLength - 2 : offset - 1) +
+                1;
+            if (filedata[cursor] == '\n') {
+              cursor += 1;
+            }
+          }
         }
         if (c == 'l') {
-          column += 1;
+          cursor += 1;
+          if (filedata[cursor] == '\n') {
+            cursor += 1;
+          }
         }
         if (c == 'h') {
-          column -= 1;
+          cursor -= 1;
+          if (filedata[cursor] == '\n') {
+            cursor -= 1;
+          }
         }
         clear();
-        print(row, column, rows, filedata);
+        print(cursor, filedata, filesize);
       }
     } else if (mode == 1) {
-      if (c == '~') {
-        mode = 0;
-      } else {
-        filedata[row][column] = c;
-        column += 1;
-        clear();
-        print(row, column, rows, filedata);
-      }
+      clear();
+      print(cursor, filedata, filesize);
     }
   }
 
